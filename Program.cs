4b07033afd4956379f69f5dbea8c85d92654ab86
v6066 <51444058+v6066@users.noreply.github.com>
2021-01-29 11:50:12 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;//c#语言的互操作服务
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();//定义一个随机数
+            int num = r.Next(0, 99);//将随机数的值赋给变量num
+            Console.WriteLine("猜数字游戏开始");//默认显示在页面上
+
+            for (; ; )
+            {
+                Console.WriteLine("输入第一个数字");
+                int i = int.Parse(Console.ReadLine());
+                if (i == num)//判断输入的值是上面生成的随机数
+                {
+                    Console.WriteLine("恭喜你，猜对了！！！");
+                    break;
+                }
+                else if (i > num)
+                {
+                    Console.WriteLine("太大了，再小一点");
+                }
+                else if (i < num)
+                {
+                    Console.WriteLine("太小了，再大一点");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
